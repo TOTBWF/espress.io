d0344348949863348f9bb20e9fc284dd8ab663a6
reedmullanix <reedmullanix@gmail.com>
2016-02-01 18:48:04 -0800

    Added some user interaction stuff to settings

--- app/src/main/java/io/reed/dripr/Views/SettingsFragment.java
+++ app/src/main/java/io/reed/dripr/Views/SettingsFragment.java
@@ -1,10 +1,13 @@
 package io.reed.dripr.Views;
 
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.support.design.widget.Snackbar;
 import android.util.Log;
 
 import io.reed.dripr.Models.SettingsHelper;
@@ -68,6 +71,9 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
             @Override
             public boolean onPreferenceClick(Preference preference) {
                 presenter.dumpDbToCsv();
+                Snackbar snackbar = Snackbar
+                        .make(getActivity().findViewById(R.id.main_coordinator_layout), "Database Dump Complete!", Snackbar.LENGTH_SHORT);
+                snackbar.show();
                 return true;
             }
         });
@@ -81,7 +87,19 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
         findPreference(SettingsHelper.CLEAR_DB_KEY).setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
             @Override
             public boolean onPreferenceClick(Preference preference) {
-                presenter.clearDb();
+                new AlertDialog.Builder(getActivity())
+                        .setTitle("Confirm Deletion?")
+                        .setMessage("This will delete all data, and cannot be undone!")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                presenter.clearDb();
+                                Snackbar snackbar = Snackbar
+                                        .make(getActivity().findViewById(R.id.main_coordinator_layout), "Cleared Database!", Snackbar.LENGTH_SHORT);
+                                snackbar.show();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, null).show();
                 return true;
             }
         });
